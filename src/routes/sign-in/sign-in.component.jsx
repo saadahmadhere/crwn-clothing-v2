a401aef45b1a3e54e806d804f9a2ef790122224a
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-12-30 23:12:09 +0530

    [feat]: sign in button

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,17 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
-	return <h3>sign in</h3>;
+	const signInHandler = async () => {
+		const response = await signInWithGooglePopup();
+		console.log(response);
+	};
+
+	return (
+		<>
+			<h3>sign in</h3>
+			<button onClick={signInHandler}>sign in with google</button>
+		</>
+	);
 };
 
 export default SignIn;
