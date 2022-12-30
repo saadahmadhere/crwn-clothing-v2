bc05fffea32269d9f36614f654dd10271fca3c28
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-12-30 18:36:10 +0530

    [update]: app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Home from './routes/home/home.componentt';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
+
+const Shop = () => <h1>this is shop</h1>;
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' index element={<Home />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
+				<Route path='sign-in' element={<SignIn />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
