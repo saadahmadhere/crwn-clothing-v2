025802fb93ff84a0ee8d207655ce87a25c2d55ac
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-12-31 00:10:06 +0530

    [feat]: createUserDocumentFromAuth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,13 @@ import {
 	GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+	getFirestore,
+	doc, //retrieve the document inside our firestore database
+	getDoc, // getting the document data
+	setDoc, // setting the document data
+} from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,3 +38,35 @@ provider.setCustomParameters({
 export const auth = getAuth();
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(); // this points to our database inside our console, which helps us further in getting/setting data.
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	// here userAuth is the response that needs to be passed after signing up with google.
+
+	const userDocRef = doc(db, 'users', userAuth.uid);
+	//console.log(userDocRef);
+	const userSnapshot = await getDoc(userDocRef);
+	//console.log(userSnapshot);
+	//console.log(userSnapshot.exists()); // this tells us whether the user is already present inside the db.
+
+	// if user data done not exist
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.error('Error creating the user.💔');
+		}
+	}
+	// if the user data exists:
+	return userDocRef;
+
+	// if it doesn't exists, then create a new user data using the above screen-shot.
+};
