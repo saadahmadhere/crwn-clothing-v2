e32a1402956aea92c1fba3460b0c647eb770a5f9
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-01-08 11:54:38 +0530

    [feat]: cart context

---
+++ src/contexts/cart.context.js
@@ -0,0 +1,14 @@
+import { useState, createContext, useContext } from 'react';
+
+const CartContext = createContext({
+	isCartOpen: false,
+	setIsCartOpen: () => {},
+});
+
+export const CartContextProvider = ({ children }) => {
+	const [isCartOpen, setIsCartOpen] = useState(false);
+	const value = { isCartOpen, setIsCartOpen };
+	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+export const useCartContext = () => useContext(CartContext);
