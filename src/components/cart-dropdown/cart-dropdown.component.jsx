16907b294e5b120c032d69f94d91fa30efaaae3a
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-01-09 11:27:24 +0530

    [feat]: add cart items in cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,22 @@
 import Button from '../button/button.component';
-// import CartItem from '../cart-item/cart-item.component';
+import { useCartContext } from '../../contexts/cart.context';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+	const { cartItems } = useCartContext();
+
 	return (
 		<div className='cart-dropdown-container'>
 			<div className='cart-items'>
-				{/* {cartItems.length ? (
+				{cartItems.length ? (
 					cartItems.map((cartItem) => (
 						<CartItem key={cartItem.id} cartItem={cartItem} />
 					))
 				) : (
 					<span className='empty-message'>Your cart is empty</span>
-				)} */}
+				)}
 			</div>
 			<Button>GO TO CHECKOUT</Button>
 		</div>
