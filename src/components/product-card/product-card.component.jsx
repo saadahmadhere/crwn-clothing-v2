e7526454b2a9d7223d8708763fc2cbe527321034
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-01-09 11:28:18 +0530

    [feat]: Add to cart button

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,12 @@
 import './product-card.styles.scss';
+import { useCartContext } from '../../contexts/cart.context';
+import Button from '../button/button.component';
 
 const ProductCard = ({ product }) => {
 	const { name, price, imageUrl } = product;
-	// const { addItemToCart } = useContext(CartContext);
+	const { addItemToCart } = useCartContext();
 
-	// const addProductToCart = () => addItemToCart(product);
+	const addProductToCart = () => addItemToCart(product);
 
 	return (
 		<div className='product-card-container'>
@@ -13,9 +15,9 @@ const ProductCard = ({ product }) => {
 				<span className='name'>{name}</span>
 				<span className='price'>{price}</span>
 			</div>
-			{/* <Button buttonType='inverted' onClick={addProductToCart}>
-				Add to card
-			</Button> */}
+			<Button buttonType='inverted' onClick={addProductToCart}>
+				Add to cart
+			</Button>
 		</div>
 	);
 };
