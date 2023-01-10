d306053c3f4255895f2c1b8636a66abecf5f071f
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-01-10 12:27:20 +0530

    [feat]: cart icon count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,13 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
 import './cart-icon.styles.scss';
+import { useCartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
+	const { cartCount } = useCartContext();
 	return (
 		<div className='cart-icon-container'>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{cartCount}</span>
 		</div>
 	);
 };
