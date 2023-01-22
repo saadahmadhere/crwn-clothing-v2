7a0e32c10fee32b2c1bcbab680398274dedf3e59
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-01-22 11:44:00 +0530

    [update]: CartIconContainer styled

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,14 @@
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import './cart-icon.styles.scss';
+// import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { ItemCount, CartIconContainer, ShoppingIcon } from './cart-icon.styles';
 import { useCartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
 	const { cartCount } = useCartContext();
 	return (
-		<div className='cart-icon-container'>
-			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>{cartCount}</span>
-		</div>
+		<CartIconContainer>
+			<ShoppingIcon />
+			<ItemCount>{cartCount}</ItemCount>
+		</CartIconContainer>
 	);
 };
 
