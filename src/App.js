9d14fcf67dd3ff1ffc29cb2e8eed3902963e2610
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-03-10 22:15:10 +0530

    setup initial store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import {
+	createUserDocumentFromAuth,
+	onAuthStateChangedListener,
+} from './utils/firebase/firebase.utils';
+import { setCurrentUser } from './store/user/user.action';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
-  );
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			dispatch(setCurrentUser(user));
+		});
+
+		return unsubscribe;
+	}, [dispatch]); //Note: dispatch can be omitted as a dependency from here as it is given by redux and it does not change.
+
+	return (
+		<Routes>
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path='shop/*' element={<Shop />} />
+				<Route path='auth' element={<Authentication />} />
+				<Route path='checkout' element={<Checkout />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
